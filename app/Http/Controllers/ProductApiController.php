f85a9178363b5b62c541e859f098503d0a63368c
ArifFadilah <ariffadilah16@gmail.com>
2021-06-13 20:59:39 +0700

    toko online dengan laravel v8

---
+++ app/Http/Controllers/ProductApiController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductApiController extends Controller
+{
+    public function index()
+    {
+        $products = product::all();
+        return response()->json(['message' => 'Success','data' => $products]);
+    }
+
+    public function show($id)
+    {
+        $product = Product::find($id);
+        return response()->json(['message' => 'Success','data' => $product]);
+    }
+
+    public function store(Request $request)
+    {
+        $product = Product::create($request->all());
+        return response()->json(['message' => 'Data has been inserted successfully','data' => $product]);
+    }
+
+    public function update(Request $request,$id)
+    {
+        $product = Product::find($id);
+        $product->update($request->all());
+        return response()->json(['message' => 'Success','data' => $product]);
+    }
+
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        $product->delete();
+        return response()->json(['message' => 'Success update','data' => null]);
+    }
+}
